32f6829e44bb7bafcddd9ee4540a537bd1d38eb5
Philip W <philip.whitt@sbcglobal.net>
2015-07-13 22:25:59 -0500

    stuff

--- src/externs/paper.js
+++ src/externs/paper.js
@@ -30,13 +30,95 @@ paper.view.onFrame = function(){};
 /** @public */
 paper.view.onResize = function(){};
 
-/** @constructor */
-paper.Path = function() {};
+/** 
+ * @constructor 
+ */
+paper.ToolEvent = function(){};
+/**
+ * @public
+ */
+paper.ToolEvent.prototype.type;
+/**
+ * @public
+ */
+paper.ToolEvent.prototype.point;
+/**
+ * @public
+ */
+paper.ToolEvent.prototype.lastPoint;
+/**
+ * @public
+ */
+paper.ToolEvent.prototype.downPoint;
+/**
+ * @public
+ */
+paper.ToolEvent.prototype.middlePoint;
+/**
+ * @public
+ */
+paper.ToolEvent.prototype.delta;
+/**
+ * @public
+ */
+paper.ToolEvent.prototype.count;
+/**
+ * @public
+ */
+paper.ToolEvent.prototype.item;
+
+/** 
+ * @constructor 
+ */
+paper.Tool = function(){};
+/** 
+ * @param {paper.ToolEvent} event
+ * @public 
+ */
+paper.Tool.prototype.onMouseDown = function(event) {}
+/** 
+ * @param {paper.ToolEvent} event
+ * @public 
+ */
+paper.Tool.prototype.onMouseDrag = function(event) {}
+/** 
+ * @param {paper.ToolEvent} event
+ * @public 
+ */
+paper.Tool.prototype.onMouseUp = function(event) {}
+
+/** 
+ * @param {Object=} options
+ * @constructor 
+ */
+paper.PointText = function(options) {};
+/** 
+ * @type {string} 
+ * @public 
+ */
+paper.PointText.prototype.content;
+
+
+/** 
+ * @param {Object=} options
+ * @constructor 
+ */
+paper.Path = function(options) {};
 /** 
  * @type {Object} 
  * @public 
  */
 paper.Path.prototype.style = {};
+/** 
+ * @type {Array} 
+ * @public 
+ */
+paper.Path.prototype.segments = [];
+/** 
+ * @type {boolean} 
+ * @public 
+ */
+paper.Path.prototype.selected;
 /** 
  * @type {Object} 
  * @public 
@@ -44,10 +126,25 @@ paper.Path.prototype.style = {};
 paper.Path.prototype.bounds = {bottomLeft:0};
 /** @public */
 paper.Path.prototype.strokeColor;
+/** @public */
+paper.Path.prototype.fullySelected;
+/** 
+ * @type {Object|string} 
+ * @public 
+ */
+paper.Path.prototype.fillColor;
 /** 
  * @public 
  */
 paper.Path.prototype.clone = function(){};
+/** 
+ * @public 
+ */
+paper.Path.prototype.simplify = function(num){};
+/** 
+ * @public 
+ */
+paper.Path.prototype.add = function(something){};
 /** 
  * @param {paper.Point} point
  * @public 
--- src/js/app/Main.js
+++ src/js/app/Main.js
@@ -9,7 +9,7 @@ app.Main = function() {
 };
 
 /** @public */
-app.Main.prototype.draw = function() {
+app.Main.prototype.example1 = function() {
 	paper.project.activeLayer.removeChildren();
 
 	var path = new paper.Path.Rectangle(new paper.Point(80, 50), new paper.Size(100, 50));
@@ -37,10 +37,52 @@ app.Main.prototype.draw = function() {
 	};
 };
 
+/** @public */
+app.Main.prototype.example2 = function() {
+	paper.project.activeLayer.removeChildren();
+
+	var path;
+	var tool = new paper.Tool();
+
+	var textItem = new paper.PointText({
+		'content': 'Click and drag to draw a line.',
+		'point': new paper.Point(20, 30),
+		'fillColor': 'black'
+	});
+
+	tool.onMouseDown = function(event) {
+		if (path) {
+			path.selected = false;
+		}
+
+		path = new paper.Path({
+			'segments': [event.point],
+			'strokeColor': 'black',
+			'fullySelected': true
+		});
+	}
+
+	tool.onMouseDrag = function(event) {
+		path.add(event.point);
+		textItem.content = 'Segment count: ' + path.segments.length;
+	}
+
+	tool.onMouseUp = function(event) {
+		var segmentCount = path.segments.length;
+		path.simplify(10);
+		path.fullySelected = true;
+
+		var newSegmentCount = path.segments.length;
+		var difference = segmentCount - newSegmentCount;
+		var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
+		textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
+	}
+};
+
 var main = new app.Main();
 var canvas = document.getElementById('canvas');
 
 paper.setup(canvas);
-main.draw();
+main.example2();
 paper.view.draw();
 paper.view.play();
